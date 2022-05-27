6408ad3785918b5afc207d0488ffdd12e2ec816b
Investigacion <betomv117@gmail.com>
2022-05-27 13:19:21 -0500

    version 2 token leido

--- app/src/main/java/com/example/firenojala/MainActivity.java
+++ app/src/main/java/com/example/firenojala/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.firenojala;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(this, new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+           //     task.getResult();
+                Log.w("Tokenmessje",  task.getResult());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/firenojala/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/firenojala/MyFirebaseMessagingService.java
@@ -1,26 +1,33 @@
 package com.example.firenojala;
 
+
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
 
+    private static final String TAG = "onNewToken";
 
 
     @Override
     public void onNewToken(String token) {
-        Log.d("token", "Refreshed token: " + token);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
+        Log.d(TAG, "Refreshed token: " + token);
 
     }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        }
 }
