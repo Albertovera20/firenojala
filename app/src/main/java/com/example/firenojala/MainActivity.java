9375908b83286828fe6bafb155328682f8ed21f1
Albertovera20 <betomv117@gmail.com>
2022-05-28 18:32:35 -0500

    version 2 token leido

--- app/src/main/java/com/example/firenojala/MainActivity.java
+++ app/src/main/java/com/example/firenojala/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -13,10 +14,22 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView infoTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infoTextView=(TextView)findViewById(R.id.nombre);
+        if (getIntent().getExtras()!=null){
+            for (String key:getIntent().getExtras().keySet()){
+                String value =getIntent().getExtras().getString(key);
+                infoTextView.append("\n"+key+": "+value);
+            }
+        }
+
         FirebaseMessaging.getInstance().getToken().addOnCompleteListener(this, new OnCompleteListener<String>() {
             @Override
             public void onComplete(@NonNull Task<String> task) {
--- app/src/main/java/com/example/firenojala/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/firenojala/MyFirebaseMessagingService.java
@@ -1,9 +1,16 @@
 package com.example.firenojala;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -28,6 +35,34 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
+ if (remoteMessage.getNotification()!=null){
+     Log.d(TAG,"NotificacionR: "+remoteMessage.getNotification().getBody());
+     mostrarNotificacion(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+ }
+
+ if(remoteMessage.getData().size()>0){
+Log.d(TAG,"DatosD"+remoteMessage.getData());
+ }
+ 
+
 
         }
+
+    private void mostrarNotificacion(String title, String body) {
+        Uri soundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Intent intent =new Intent(this,MainActivity.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent=PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder=new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+        .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(soundUri)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
+
+    }
 }
